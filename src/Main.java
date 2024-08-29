883a0fa28caaa7e2e20c1f1c0c662ed82304b958
mariyamango <de.msomkova@gmail.com>
2024-08-29 16:10:55 +0200

    added dates printing

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,21 @@ public class Main {
     public static void main(String[] args) {
         String[] months = new String[] {"january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"};
         for (String month : months) {
-            printCalendarForMonth(month);
+            printCalendarForMonth(month,30);
         }
     }
 
-    public static void printCalendarForMonth(String monthName) {
+    public static void printCalendarForMonth(String monthName, int daysInMonth) {
         System.out.println(monthName);
 
         System.out.println("Mo Tu We Th Fr Sa Su");
-        System.out.println("1  2  3  4  5  6  7");
 
+        for (int i = 1; i <= daysInMonth; i++) {
+            System.out.print(i + "  ");
+            if (i % 7 == 0) {
+                System.out.println();
+            }
+        }
+        System.out.println();
     }
 }
