9aa1574a7d446190b391b268852344769bcb82b2
sama-hh <samahajimuradovah@gmail.com>
2024-08-29 15:55:00 +0200

    added 4th and 5th tasks

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,9 @@ public class Main {
 
     public static void printCalendarForMonth(String monthName) {
         System.out.println(monthName);
+
+        System.out.println("Mo Tu We Th Fr Sa Su");
+        System.out.println("1  2  3  4  5  6  7");
+
     }
 }
